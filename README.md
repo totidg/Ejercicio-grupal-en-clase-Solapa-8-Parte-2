9d5c85ab88dab94d89648102fe46362dbc1012da
totidg <113801644+totidg@users.noreply.github.com>
2023-03-23 23:48:08 -0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# Ejercicio-grupal-en-clase-Solapa-8-Parte-2
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/web-platform-expxsi)
---
+++ script.js
@@ -0,0 +1,36 @@
+
+
+const CONTENEDOR = document.getElementById('conteiner');
+
+
+
+function suma() {
+  let numero1 = document.getElementById('numero1').value;
+  let numero2 = document.getElementById('numero2').value;
+  const TOTAL = +numero1 + +numero2;
+   CONTENEDOR.innerHTML = 'La suma de los números es: ' + TOTAL;
+}
+
+function resta() {
+  let numero1 = document.getElementById('numero1').value;
+  let numero2 = document.getElementById('numero2').value;
+  const TOTAL = +numero1 - +numero2;
+  CONTENEDOR.innerHTML = 'La resta de los números es: ' + TOTAL;
+}
+
+function multiplicasion() {
+  let numero1 = document.getElementById('numero1').value;
+  let numero2 = document.getElementById('numero2').value;
+  const TOTAL = +numero1 * +numero2;
+  CONTENEDOR.innerHTML = 'El producto de los números es: ' + TOTAL;
+}
+
+function division() {
+  let numero1 = document.getElementById('numero1').value;
+  let numero2 = document.getElementById('numero2').value;
+  const TOTAL = +numero1 / +numero2;
+  CONTENEDOR.innerHTML = 'El cociente de los números es: ' + TOTAL;
+  if (numero2 == 0) {
+    CONTENEDOR.innerHTML = 'No es posible dividir por 0';
+  }
+}
